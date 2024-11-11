e7508f93afcb5ea9d749d398e67972202ee86fc8
Duynhat989 <vietduy989kc@gmail.com>
2024-11-11 15:05:27 +0700

    update api 2

--- src/components/AddAssistantPopup.vue
+++ src/components/AddAssistantPopup.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps, defineEmits, onMounted, watch } from 'vue';
+import { ref, defineEmits, onMounted, watch } from 'vue';
 
 const emit = defineEmits(['close', 'submit']);
 import { notify } from '@kyvg/vue3-notification';
